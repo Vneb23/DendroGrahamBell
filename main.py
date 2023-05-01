7c20f87f279409fea57d066da45f66953fd42b26
Adrien <decosse.adrien@gmail.com>
2023-05-01 22:09:49 +0200

    correction main

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def create_clusters(n: int) -> list[Cluster]:
 
 def main(n: int, dist: str ="min") -> int:
     L_cluster = create_clusters(n)
-    img = imread(choose_one())
-    print(find_cluster(L_cluster, from_image_to_array(img), dist=dist))
+    path_img = choose_one()
+    img = imread(path_img)
+    print(find_cluster(L_cluster, from_image_to_array(path_img), dist=dist).int)
     plt.imshow(img, cmap="gray")
     plt.show()
 
