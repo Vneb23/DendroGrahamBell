2dd67a13e3ff03e951c68c3f33f5403b0a30426d
lauragrh <lauragrehaigne@gmail.com>
2023-05-01 23:04:16 +0200

    changed for new dataset

--- choose.py
+++ choose.py
@@ -7,10 +7,10 @@ def choose(n: int) -> list[list[str]]:
     """Chosses randomly n images for each numbers"""
     L_dir = []
     for i in range(10):
-        l = os.listdir(f"numbers/{i}")
+        l = os.listdir(f"Numbers_bis/{i}")
         L_dir.append([])
         for j in range(n):
-            L_dir[-1].append(f"numbers/{i}/{l[rd.randint(0,len(l)-1)]}")
+            L_dir[-1].append(f"Numbers_bis/{i}/{l[rd.randint(0,len(l)-1)]}")
     return L_dir
 
 
@@ -26,10 +26,5 @@ def from_image_to_array (name):
           name of the image
     """
     img = imread(name)
-    n,m,t = img.shape
-    img_bw = np.zeros((n,m))
-    for i in range(n):
-        for j in range(m):
-            img_bw[i][j] = int(np.mean(img[i][j]))
-    return img_bw.ravel()
+    return img.ravel()
 
